dd226a963d2c2eb48710a8e9853c900deaa8acee
EdsKelvin <edsondev20@gmail.com>
2023-11-28 22:21:20 -0300

    tela de login/registro

---
+++ front/src/pages/Signup/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import * as C from "./styles";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [user_name, setUser_name] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { signup } = useAuth();
+
+  const handleSignup = () => {
+    if (!email || !user_name || !senha) {
+      setError("Preencha todos os campos");
+      return;
+    } 
+
+    const res = signup(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("Usuário cadastrado com sucesso!");
+    navigate("/");
+  }
+
+  return (
+    <C.Container>
+      <C.Label>Registro</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setError("");
+          }}
+        />
+        <Input
+          type="user_name"
+          placeholder="Digite seu Usuario"
+          value={user_name}
+          onChange={(e) => {
+            setUser_name(e.target.value);
+            setError("");
+          }}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={senha}
+          onChange={(e) => {
+            setSenha(e.target.value);
+            setError("");
+          }}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Inscrever-se" onClick={handleSignup} />
+        <C.LabelSignin>
+          Já tem uma conta?
+          <C.Strong>
+            <Link to="/">&nbsp;Entre</Link>
+          </C.Strong>
+        </C.LabelSignin>
+      </C.Content>
+    </C.Container>
+  );
+}
+
+export default Signup;
